c0083b866b5935d546ad5fe98dc3e5b7af45176c
Justin Miller <jayman1294@gmail.com>
2021-07-19 23:49:16 -0400

    created index and show actions for users-controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,11 @@
 class UsersController < ApplicationController
   def index
-
+    @users = User.all
+    render json: @users
   end
 
   def show 
-  end
-
-  def create
-
-  end
-
-  def update
-  end
-
-  def destroy
+    @user = User.find_by(id: params[:id])
+    render json: @user
   end
 end
