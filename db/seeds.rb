469f5571c0f9d0f2fd8c791bae2d492f31308b03
Justin Miller <jayman1294@gmail.com>
2021-09-25 20:54:31 -0400

    added images to events in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -62,6 +62,21 @@ mentor2 = Mentor.create(
 
 
 event1 = Event.create(name: "Meet Up",date: 'Tomorrow',time: "10:00 AM",description: "Event for members to meetup with mentors",location: "New York", likes: 17)
+event2 = Event.create(name: "Networking", date: 'Today', time: "12:00 PM", description: "Grab some drinks and meet with other professionals anbd mentors", location: "Atlanta", likes: 45)
+event3 = Event.create(name: "Meet Up",date: 'Aug 30',time: "11:00 AM",description: "Meetup with mentors",location: "New York", likes: 25)
+event4 = Event.create(name: "Info Session", date: 'Aug 31', time: "1:00 PM", description: "Info session on programs we offer", location: "Atlanta", likes: 13)
+event5 = Event.create(name: "Counseling",date: 'Sept 4',time: "10:00 AM",description: "Free counseling",location: "Chicago", likes: 5)
+event6 = Event.create(name: "Meet and Greet", date: 'Sept 2', time: "2:00 PM", description: "Grab some drinks and meet with other professionals anbd mentors", location: "Atlanta", likes: 1)
+event7 = Event.create(name: "Info Session",date: 'Sept 4',time: "7:00 AM",description: "Info session on programs we offer",location: "Los Angeles", likes: 33)
+event8 = Event.create(name: "Brunch",date: 'Sept 3',time: "11:00 AM",description: "Come have brunch with fellow mentees and network and make friends",location: "Chicago", likes: 30)
+event9 = Event.create(name: "Mini Golf",date: 'Aug 29',time: "3:00 PM",description: "Come play mini golf at this fun event hosted downtown",location: "Atlanta", likes: 10)
+event10 = Event.create(name: "Meet Up",date: 'Sept 1',time: "9:00 AM",description: "Meetup with mentors",location: "New York", likes: 0)
+event11 = Event.create(name: "Job Fair",date: 'Sept 1',time: "8:00 PM",description: "The biggest job fair in the city, Meet and talk with recruiters and company reps",location: "New York", likes: 20)
+event12 = Event.create(name: "Career Day",date: 'Sept 3',time: "9:00 AM",description: "Find out about all the potential career paths that are available and in demand",location: "Chicago", likes: 59)
+event13 = Event.create(name: "Meet Up",date: 'Today',time: "5:00 PM",description: "Meetup with potential mentors",location: "New York", likes: 31)
+event14 = Event.create(name: "Mentor Panel",date: 'Tomorrow',time: "8:00 AM",description: "Our yearly panel where mentors give advice on how they got to where they are",location: "New York", likes: 10)
+event15 = Event.create(name: "Brunch",date: 'Today',time: "12:00 PM",description: "Come eat and have a good time",location: "New York", likes: 12)
+event16 = Event.create(name: "Info Session",date: 'Tomorrow',time: "11:00 AM",description: "Info session on programs we offer",location: "New York", likes: 10)
 
 
 event1.image.attach(
@@ -69,6 +84,82 @@ event1.image.attach(
   filename: 'meetup.jpg',
   content_type: 'application/jpg'
 )
+event2.image.attach(
+  io: File.open('./app/assets/images/networking.jpg'),
+  filename: 'networking.jpg',
+  content_type: 'application/jpg'
+)
+event3.image.attach(
+  io: File.open('./app/assets/images/meetup.jpg'),
+  filename: 'meetup.jpg',
+  content_type: 'application/jpg'
+)
+event4.image.attach(
+  io: File.open('./app/assets/images/info-session.jpg'),
+  filename: 'info-session.jpg',
+  content_type: 'application/jpg'
+)
+event5.image.attach(
+  io: File.open('./app/assets/images/counseling.png'),
+  filename: 'counseling.png',
+  content_type: 'application/png'
+)
+event6.image.attach(
+  io: File.open('./app/assets/images/meet-greet.jpg'),
+  filename: 'meet-greet.jpg',
+  content_type: 'application/jpg'
+)
+event7.image.attach(
+  io: File.open('./app/assets/images/info-session.jpg'),
+  filename: 'info-session.jpg',
+  content_type: 'application/jpg'
+)
+event8.image.attach(
+  io: File.open('./app/assets/images/brunch.jpg'),
+  filename: 'brunch.jpg',
+  content_type: 'application/jpg'
+)
+event9.image.attach(
+  io: File.open('./app/assets/images/golf.jpg'),
+  filename: 'golf.jpg',
+  content_type: 'application/jpg'
+)
+event10.image.attach(
+  io: File.open('./app/assets/images/meetup.jpg'),
+  filename: 'meetup.jpg',
+  content_type: 'application/jpg'
+)
+event11.image.attach(
+  io: File.open('./app/assets/images/job-fair.jpg'),
+  filename: 'job-fair.jpg',
+  content_type: 'application/jpg'
+)
+event12.image.attach(
+  io: File.open('./app/assets/images/career.jpg'),
+  filename: 'career.jpg',
+  content_type: 'application/jpg'
+)
+event13.image.attach(
+  io: File.open('./app/assets/images/meetup.jpg'),
+  filename: 'meetup.jpg',
+  content_type: 'application/jpg'
+)
+event14.image.attach(
+  io: File.open('./app/assets/images/panel.jpg'),
+  filename: 'panel.jpg',
+  content_type: 'application/jpg'
+)
+event15.image.attach(
+  io: File.open('./app/assets/images/brunch.jpg'),
+  filename: 'brunch.jpg',
+  content_type: 'application/jpg'
+)
+event16.image.attach(
+  io: File.open('./app/assets/images/info-session.jpg'),
+  filename: 'info-session.jpg',
+  content_type: 'application/jpg'
+)
+
 
 =begin booking1 = Booking.create(date: 'today', user: user1, mentor: mentor1)
 booking2 = Booking.create(date: 'today', user: user1, mentor: mentor2) 
