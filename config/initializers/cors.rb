48f5028da636f811ce0b537c66b6cd16e117a2ac
Justin Miller <jayman1294@gmail.com>
2021-10-08 03:55:15 -0400

    fixed CORS error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://my-career-builder-app.herokuapp.com/'
+    origins '*'
     resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head],
     credentials: true
   end
